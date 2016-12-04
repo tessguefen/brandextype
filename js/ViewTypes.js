4e0fe0e0401395b85b93d9a337dd726a59d5b142
Tess <tguefen@gmail.com>
2016-12-03 19:14:53 -0800

    Add files via upload

--- js/ViewTypes.js
+++ js/ViewTypes.js
@@ -1,23 +1,37 @@
 function ViewTypes_Batchlist() {
-	MMBatchList.call( this, 'BXTTypes' );
-	this.Feature_SearchBar_SetPlaceholderText( 'Search Types...' );
+	MMBatchList_AssignList.call( this, 'BXTView_Types', true );
+	this.Feature_SearchBar_SetPlaceholderText( 'Search Categories...' );
 	this.SetDefaultSort( 'id', '' );
 }
 
-DeriveFrom( MMBatchList, ViewTypes_Batchlist );
+DeriveFrom( MMBatchList_AssignList, ViewTypes_Batchlist );
 
 ViewTypes_Batchlist.prototype.onLoad = ViewTypes_List_Load_Query;
+
+ViewTypes_Batchlist.prototype.onLoad = function( filter, sort, offset, count, callback, delegator ) {
+	ViewTypes_List_Load_Query( this.load_assigned, this.load_unassigned, filter, sort, offset, count, callback, delegator );
+}
 
 ViewTypes_Batchlist.prototype.onCreateRootColumnList = function() {
 	var columnlist =
 	[
-		new MMBatchList_Column_Name( 'Category ID', 'cat_id', 'cat_id')
+		new MMBatchList_Column_Name( 'Type ID', 'type_id', 'type_id')
+		.SetDisplayInMenu(false)
+		.SetDisplayInList(false)
+		.SetAdvancedSearchEnabled(false),
+		new MMBatchList_Column_Name( 'Category ID', 'category_id', 'category_id')
 		.SetDisplayInMenu(false)
 		.SetDisplayInList(false)
 		.SetAdvancedSearchEnabled(false),
 		new MMBatchList_Column_CheckboxSlider('Assigned', 'assigned', 'assigned', function( item, checked, delegator ) { ViewTypes_Batchlist.Update_Approved( item, checked, delegator ); } ),
 		new MMBatchList_Column_Text( 'Category Code', 'category_code', 'category_code' ),
-		new MMBatchList_Column_Text( 'Category Name', 'category_name', 'category_name' )
+		new MMBatchList_Column_Text( 'Category Name', 'category_name', 'category_name' ),
+		new MMBatchList_Column_Text( 'Category Active', 'category_active', 'category_active' )
 	];
 	return columnlist;
+}
+
+// On Toggle'd Approved
+ViewTypes_Batchlist.Update_Approved = function( item, checked, delegator ) {
+	ViewTypes_Batchlist_Approved( item, checked, function( response ) {}, delegator );
 }
--- js/ViewTypes_functions.js
+++ js/ViewTypes_functions.js
@@ -1,21 +1,25 @@
-function ViewTypes_List_Load_Query( filter, sort, offset, count, callback, delegator ) {
+function ViewTypes_List_Load_Query( assigned, unassigned, filter, sort, offset, count, callback, delegator ) {
 	return AJAX_Call_Module(	callback,
 								'admin',
-								'Typeextype',
+								'brandextype',
 								'ViewTypes_Load_Query',
+								'&Type_ID=' + <MvEVAL EXPR = "{ g.View_Type }"> +
 								'&Filter=' + EncodeArray( filter ) +
 								'&Sort=' + encodeURIComponent( sort ) +
 								'&Offset=' + encodeURIComponent( offset ) +
+								'&Assigned=' + ( assigned ? '1' : '0' ) +
+					  			'&Unassigned='	+ ( unassigned ? '1' : '0' ) +
 								'&Count=' + encodeURIComponent( count ),
 								delegator );
 }
 // On Approved
-function ViewTypes_Batchlist_Approved ( id, checked, callback, delegator ) {
+function ViewTypes_Batchlist_Approved ( item, checked, callback, delegator ) {
 	return AJAX_Call_Module(	callback,
 								'admin',
-								'Typeextype',
+								'brandextype',
 								'TypeXCategoryAssign',
-								'Category_ID='		+ encodeURIComponent( id ) +
+								'Category_ID='		+ encodeURIComponent( item.record.category_id ) +
+								'&Type_ID=' + encodeURIComponent( item.record.type_id ) +
 								'&Assign='	+ ( checked ? 1 : 0 ),
 								delegator );
 }
